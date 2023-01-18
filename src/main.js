f5221f6bcd54151344aba0ba6818d42b6a53b399
kagsburg <kagsburg@gmail.com>
2023-01-18 08:38:21 +0300

    removed icons

--- src/main.js
+++ src/main.js
@@ -3,15 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
-import { OhVueIcon, addIcons } from "oh-vue-icons";
 
-import { FaFlag, RiZhihuFill, HiX } from "oh-vue-icons/icons";
-
-
-
-addIcons(FaFlag, RiZhihuFill,HiX);
-
-Vue.component("v-icon", OhVueIcon);
 
 
 new Vue({
